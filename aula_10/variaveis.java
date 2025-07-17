b3d8992a671199bd04659cfc3b5b763af280776f
Walt <waltjohnsley@ufpr.br>
2025-07-16 21:26:58 -0300

    aula 10 - variaveis

---
+++ aula_10/variaveis.java
@@ -0,0 +1,10 @@
+class Variaveis {
+    public static void main (String[] args) {
+        int idade;
+        String nome;
+        String nomeCachorro;
+
+        System.out.println ("Variaveis");
+        System.out.println ("Walt tem " + idade + " anos");
+    }
+}
